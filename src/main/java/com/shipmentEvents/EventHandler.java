e9f94688a3ce610764c534512043dab22ed3abb1
pats123in <67869839+pats123in@users.noreply.github.com>
2021-12-14 15:18:03 -0700

    Update EventHandler.java
    Return Static Client

--- src/main/java/com/shipmentEvents/EventHandler.java
+++ src/main/java/com/shipmentEvents/EventHandler.java
@@ -186,7 +186,7 @@ public class EventHandler implements RequestHandler<ScheduledEvent, String> {
     }
     public static AmazonS3 amazonS3 = AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
     public static AmazonS3 getS3Client() {
-        return AmazonS3ClientBuilder.standard().withRegion(Regions.DEFAULT_REGION).build();
+        return amazonS3;
     }
     
     
